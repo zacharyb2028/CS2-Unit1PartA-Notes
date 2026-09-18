5d92695136d0000555b2d7dc68a38394ee283f4d
zacharyb2028 <zachary.belitz2028@gbwl.org>
2026-09-18 17:56:03 +0000

    Tested printing lines

--- Main.java
+++ Main.java
@@ -5,9 +5,11 @@ This is my comment space!
 
 
 
+
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      System.out.println("It makes no sense to divide by zero!");
+      System.out.println(0/3);
    }
 }
