aab088859e0143cdb13564f2936a1d111c4d952e
zacharyb2028 <zachary.belitz2028@gbwl.org>
2026-09-23 19:07:00 +0000

    learned printing rules

--- Main.java
+++ Main.java
@@ -9,7 +9,12 @@ This is my comment space!
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("It makes no sense to divide by zero!");
-      System.out.println(0/3);
+      int num;
+      num = 4;           
+      System.out.println(num);
+      
+
    }
 }
+
+
